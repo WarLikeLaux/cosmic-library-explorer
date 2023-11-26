47ad8989198fe16c58588542d5b30baccb9ac699
WarLikeLaux <warlikelaux080399@mail.ru>
2023-11-26 09:28:43 +0600

    Added downloading books with titles

--- main.py
+++ main.py
@@ -1,8 +1,57 @@
 import os
 import pathlib
+import urllib
 
 import dotenv
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+
+BASE_URL = "https://tululu.org"
+
+
+def get_book_details_from_response(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    title, author = [element.strip()
+                     for element in soup.find('h1').text.split("::")]
+    return title, author
+
+
+def get_url_for_scraping(id):
+    safe_id = urllib.parse.quote(str(id))
+    return f"{BASE_URL}/b{safe_id}/"
+
+
+def get_url_for_download_txt(id):
+    url = f"{BASE_URL}/txt.php"
+    params = {
+        "id": id
+    }
+    parsed_url = urllib.parse.urlparse(url)
+    encoded_params = urllib.parse.urlencode(params)
+    full_url = urllib.parse.urlunparse(
+        parsed_url._replace(query=encoded_params)
+    )
+    return full_url
+
+
+def download_txt(url, filename, folder='books/'):
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+
+    filename_without_extension, _ = os.path.splitext(filename)
+    cleaned_filename = sanitize_filename(filename_without_extension)
+    final_filename = cleaned_filename + ".txt"
+
+    filepath = os.path.join(folder, final_filename)
+
+    with open(filepath, "wb") as file:
+        file.write(response.content)
+
+    return filepath
 
 
 def check_for_redirect(response):
@@ -16,22 +65,31 @@ def main():
 
     books_directory_abs_path = os.path.join(os.getcwd(), books_directory)
     pathlib.Path(books_directory_abs_path).mkdir(parents=True, exist_ok=True)
+
     start_id = 1
-    for i in range(10):
-        params = {
-            "id": start_id + i
-        }
-        url = "https://tululu.org/txt.php"
-        response = requests.get(url, params=params)
+
+    for increment in range(10):
+        current_id = start_id + increment
+
+        url = get_url_for_scraping(current_id)
+        response = requests.get(url)
         response.raise_for_status()
         try:
             check_for_redirect(response)
         except requests.HTTPError:
             continue
-        filename = f"id{start_id + i}.txt"
-        filepath = os.path.join(books_directory_abs_path, filename)
-        with open(filepath, "wb") as file:
-            file.write(response.content)
+        book_title, book_author = get_book_details_from_response(response)
+
+        try:
+            downloaded = download_txt(
+                url=get_url_for_download_txt(current_id),
+                filename=f"{current_id}. {book_title}.txt",
+                folder=books_directory_abs_path,
+            )
+        except requests.HTTPError:
+            continue
+
+        print(downloaded)
 
 
 if __name__ == "__main__":
